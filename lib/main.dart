81587742ede220fa0b5b2a2ea6f6412c89281eee
Mostafa Mahmoud Salem <msalem@ejada.com>
2020-11-10 22:00:43 +0200

    feat: completed app functionality, generating random dice upon pressing on image flat buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,7 +17,22 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber;
+  int rightDiceNumber;
+
+  @override
+  void initState() {
+    super.initState();
+    leftDiceNumber = 1;
+    rightDiceNumber = 2;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -25,19 +42,30 @@ class DicePage extends StatelessWidget {
           children: <Widget>[
             Expanded(
               child: FlatButton(
-                onPressed: () {},
-                child: Image.asset('images/dice1.png'),
+                onPressed: () {
+                  generateRandomDiceNumbers();
+                },
+                child: Image.asset('images/dice$leftDiceNumber.png'),
               ),
             ),
             Expanded(
               child: FlatButton(
-                onPressed: () {},
-                child: Image.asset('images/dice2.png'),
+                onPressed: () {
+                  generateRandomDiceNumbers();
+                },
+                child: Image.asset('images/dice$rightDiceNumber.png'),
               ),
             ),
           ],
         ),
       ),
     );
+  }
+
+  void generateRandomDiceNumbers() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
   }
 }
